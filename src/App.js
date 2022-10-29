f533a562bf2c75f162c492d6fc6358e269041a1c
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-10-29 01:32:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Contacts } from "./pages/Contacts";
-import { Home } from "./pages/Home";
-import { Notfound } from "./pages/Notfound";
-// import Users from "./pages/Users";
-import { Socials } from "./pages/Socials";
-import { Physical } from "./pages/Physical";
-const UsersLazyLoad = React.lazy(() => import("./pages/Users"));
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import './App.css';
+import Fallback from './pages/Fallback';
+
+import Home from './pages/Home';
+import Notfound from './pages/Notfound';
+
+const UsersLazyLoad = React.lazy(() => import('./pages/Users'));
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <ul className="App-nav-li">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+	return (
+		<div className="App">
+			<ErrorBoundary FallbackComponent={Fallback}>
+				<ul className="App-nav-li">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
 
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/contacts">Contacts</Link>
-          </li>
-        </ul>
-      </nav>
+					<li>
+						<Link to="/users">Users</Link>
+					</li>
+				</ul>
+				<nav />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/users"
-          element={
-            <React.Suspense fallback="LOADING......">
-              <UsersLazyLoad />
-            </React.Suspense>
-          }
-        />
-        <Route path="/contacts">
-          <Route index element={<Contacts />} />
-          <Route path="socials" element={<Socials />} />
-          <Route path="physical" element={<Physical />} />
-        </Route>
-
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </div>
-  );
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/users"
+						element={
+							<React.Suspense fallback="LOADING......">
+								<UsersLazyLoad />
+							</React.Suspense>
+						}
+					/>
+					<Route path="*" element={<Notfound />} />
+				</Routes>
+			</ErrorBoundary>
+		</div>
+	);
 }
 
 export default App;
